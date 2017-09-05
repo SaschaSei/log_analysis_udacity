a5a48777f15c751a35f36f80f76cb7a378ccb29c
SaschaSei <saschasei@googlemail.com>
2017-09-05 15:21:13 +0200

    Add routine to check for and create needed Views in database.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+# Log Analysis for Udacity Full Stack Web Dev
+
+This program is written for the Udacity Full Stack Web Dev Nanodegree. It uses
+a database provided by Udacity and outputs three pieces of data.
+
+## Prerequisits
+- the program is written in Python 3.6.2 downloadable [here](https://www.python.org/downloads/)
+- you will also need to install [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+- and [Vagrant](https://www.vagrantup.com/downloads.html)
+
+## How to run the program
+
+- install VirtualBox
+- install Vagrant
+https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+
 create view article_count as select articles.title, count(*) from articles inner join log on log.path like concat('%', articles.slug, '%') where log.status = '200 OK' group b
 y articles.title order by count desc;
 
--- log_analysis.py
+++ log_analysis.py
@@ -26,20 +26,38 @@ query_3 = ("select daily_error_count.time,  "
             "100 / daily_request_count.count) >=1 "
             "order by daily_error_count.time;")
 
+# First view needed in database and the query to create it.
+view_one = ["daily_error_count",
+                "create view daily_error_count as select cast(time as date), "
+                "status, count(*) from log "
+                "where log.status like concat(4, '%') "
+                "group by cast(time as date), status "
+                "order by cast(time as date) desc;"]
 
+# Second view needed in database and query to create it
+view_two = ["daily_request_count",
+            "create view daily_request_count as select cast(time as date), "
+            "count(*) from log group by cast(time as date) "
+            "order by cast(time as date) desc;"]
+
+
+
+# Establishes connection to DB throws error if failing.
 def connect_to_DB(db_name = DB_NAME):
     try:
         db = psycopg2.connect(database=DB_NAME)
-        cur = db.cursor()
-        return db, cur
+        cursor = db.cursor()
+        return db, cursor
     except:
         print ("Unable to connect.")
 
+# Gets query returns results from DB.
 def get_query_results(query):
     db, cursor = connect_to_DB()
     cursor.execute(query)
     return cursor.fetchall()
     db.close()
+
 
 def print_results(query_results):
     index = 1
@@ -47,15 +65,44 @@ def print_results(query_results):
         print index,'. ', str(title),'\t -','\t', str(results), 'Views'
         index += 1
 
+
 def print_error_result(query_result):
     for date, perc in query_result:
         print date, ' had ', perc, '% Errors'
 
+
+def create_view(view):
+    if (not check_for_view(view[0])):
+        db, cursor = connect_to_DB()
+        cursor.execute(view[1])
+        db.commit()
+        db.close()
+        print "{} view created".format(view[0])
+    else:
+        print "{} view found".format(view[0])
+
+
+def check_for_view(view_name):
+    db, cursor = connect_to_DB()
+    cursor.execute("select exists(select * from information_schema.tables "
+                    "where table_name = '{}')".format(view_name))
+    return cursor.fetchone()[0]
+    db.close()
+
+
 if __name__ == '__main__':
+
+    create_view(view_one)
+    create_view(view_two)
+
+    
+    # Get data to print out.
     top_three_articles = get_query_results(query_1)
     most_popular_authors = get_query_results(query_2)
     error_days = get_query_results(query_3)
 
+
+    # Print data.
     print "\n Top Three Articles: \n"
     print_results(top_three_articles)
 
